5cc1e5ae9e72583bfb42dbd140edc85a7fa77b42
ArunaluB <IT22515612@my.sliit.lk>
2024-10-02 13:15:39 +0530

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require("dotenv").config();
 const ImageKit = require('imagekit');
-
+const mongoose = require('mongoose');
 const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
@@ -23,7 +23,71 @@ app.get("/api/upload", (req, res) => {
     res.send(result);
 });
 
+
+app.post("/api/chats", async (req, res) => {
+    const userId = req.auth.userId;
+    const { text } = req.body;
+
+    try {
+        // CREATE A NEW CHAT
+        const newChat = new Chat({
+            userId: userId,
+            history: [{ role: "user", parts: [{ text }] }],
+        });
+
+        const savedChat = await newChat.save();
+
+        // CHECK IF THE USERCHATS EXISTS
+        const userChats = await UserChats.find({ userId: userId });
+
+        // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+        if (!userChats.length) {
+            const newUserChats = new UserChats({
+                userId: userId,
+                chats: [
+                    {
+                        _id: savedChat._id,
+                        title: text.substring(0, 40),
+                    },
+                ],
+            });
+
+            await newUserChats.save();
+        } else {
+            // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+            await UserChats.updateOne(
+                { userId: userId },
+                {
+                    $push: {
+                        chats: {
+                            _id: savedChat._id,
+                            title: text.substring(0, 40),
+                        },
+                    },
+                }
+            );
+
+            res.status(201).send(newChat._id);
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("Error creating chat!");
+    }
+});
+
+
+
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO);
+        console.log("Connected to MongoDB");
+    } catch (err) {
+        console.log(err);
+    }
+};
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    connect();
 });
 
